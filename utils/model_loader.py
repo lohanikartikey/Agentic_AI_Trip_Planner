c3b0adc4e535cef12202f9b499e38f3b27b655ce
Kartikey Lohani <kartikey.lohani@gmail.com>
2026-03-10 20:36:51 +0530

    model_loader made

---
+++ utils/model_loader.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+from typing import Literal, Optional, Any
+from pydantic import BaseModel, Field
+from utils.config_loader import load_config
+from langchain_groq import ChatGroq
+
+
+class ConfigLoader:
+    def __init__(self):
+        print("Loading configuration...")
+        self.config = load_config()
+    
+    def __getitem__(self, key):
+        return self.config[key]
+
+
+class ModelLoader:
+    model_provider: Literal["groq", "openai"]="groq"
+    config: Optional[ConfigLoader]=Field(default=None, exclude=True)
+
+    def model_post_init(self):
+        self.config = ConfigLoader()
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def load_llm(self):
+        """
+        Load and return the LLM model
+        """
+        print("LLM Loading...")
+        print(f"Loading mocel from provider:{self.model_provider}")
+        if self.model_provicer == 'groq':
+            groq_api_key = os.getenv("GROQ_API_KEY")
+            model_name = self.config["llm"]["groq"]["model_name"]
+            llm = ChatGroq(model=model_name, api_key=groq_api_key)
+        else:
+            raise ValueError(f"Unsupported model provider: {self.model_provider}")
+        print("LLM Loaded successfully")
+
+        return llm
